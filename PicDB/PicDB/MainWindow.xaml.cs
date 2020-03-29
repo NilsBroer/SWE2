ba88f22f37926f95fe360216b5fa4acce57e6174
Nils B <44638127+NilsBroer@users.noreply.github.com>
2020-03-29 11:59:07 +0200

    First attempts at binding :)

--- PicDB/PicDB/MainWindow.xaml.cs
+++ PicDB/PicDB/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PicDB.Models;
 
 namespace PicDB
 {
@@ -23,6 +24,35 @@ namespace PicDB
         public MainWindow()
         {
             InitializeComponent();
+
+            String Path = @"C:\Users\Nils\Google Drive\UNI\Semester 04\SWE2\PicDB\PicDB";
+            
+            List<string> fileNames = new List<string>(System.IO.Directory.EnumerateFiles(Path + "/images/pokemon/","*.png")); //TODO: Adapt search pattern to jp(e)g
+
+            foreach (string fileName in fileNames)
+            {
+                Image image = new Image
+                {
+                    Source = new BitmapImage(new Uri(fileName)),
+                    Height = 50
+                };
+                Border border = new Border();
+                ImageHolder.Children.Add(image);
+            }
+
+            Models.ComboBox CBO1_DC = new Models.ComboBox()
+            {
+                Name = "Select License",
+                Options = new List<string>()
+                {
+                    "Option 1",
+                    "Option 2",
+                    "Option 3",
+                    "I don't have a license"
+                }
+            };
+
+            CB01.DataContext = CBO1_DC;
         }
     }
 }
