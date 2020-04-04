4434175d5d8fc08568b05e2ed0cd0d056634283a
Chris Kunerth <44638104+ChristophKunerth@users.noreply.github.com>
2020-04-04 17:46:05 +0200

    MainWindow now loads the UI from a bunch of split-up sub-windows. Simply Visual, no functionality yet implemented.

--- PicDB/PicDB/Windows/FileWindow.xaml.cs
+++ PicDB/PicDB/Windows/FileWindow.xaml.cs
@@ -1,34 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 using PicDB.Internal;
 using PicDB.Models;
+
 namespace PicDB
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FileWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FileWindow : UserControl
     {
-        public MainWindow()
+        public FileWindow()
         {
             InitializeComponent();
             String Path;
 
-            if (System.IO.Directory.GetCurrentDirectory().StartsWith(@"C:\Users\Chris"))    
+            if (System.IO.Directory.GetCurrentDirectory().StartsWith(@"C:\Users\Chris"))
                 Path = @"C:\Users\Chris\source\repos\SWE2\PicDB\PicDB";
             else
                 Path = @"C:\Users\Nils\Google Drive\UNI\Semester 04\SWE2\PicDB\PicDB";
 
             List<string> fileNames =
                 new List<string>(
-                    System.IO.Directory.EnumerateFiles(Path + "/images/pokemon/",
-                        "*.png")); //TODO: Adapt search pattern to include jp(e)g, then change to relevant pictures (DB?)
-            
+                    System.IO.Directory.EnumerateFiles(Path + "/images/pokemon/", "*.png")); //TODO: Adapt search pattern to include jp(e)g, then change to relevant pictures (DB?)
+
             foreach (string fileName in fileNames)
             {
                 Image image = new Image
@@ -38,7 +45,7 @@ namespace PicDB
                 };
                 ImageHolder.Children.Add(image);
             }
-            
+
             Objects.ComboBox labelBoxDataContext = new Objects.ComboBox()
             {
                 Name = "Select License",
@@ -53,14 +60,9 @@ namespace PicDB
 
             LabelBox.DataContext = labelBoxDataContext;
 
-            //Following loads simple query into DataTable and displays it in Help via Binding TODO: DELETE, after using somewhere else
-
+            //Load all photographer names into list and apply as ItemsSource, TODO: CAN STAY
             SqlCommand command = DBhelper.Create_Command("SELECT * FROM Fotografen");
             DataTable table = DBhelper.Get_DataTable(command);
-
-            MyDataGrid.ItemsSource = table.DefaultView;
-
-            //Load all photographer names into list and apply as ItemsSource, TODO: CAN STAY
 
             List<String> photographerNames = new List<string>();
             for (int i = 0; i < table.Rows.Count - 1; i++)
@@ -70,21 +72,6 @@ namespace PicDB
             }
 
             PhotographerBox.ItemsSource = photographerNames;
-
-            //Get a Photographer by ID (Just tested whether the DataAccessLayer worked with this one)
-
-            PhotographerModel photographer = PhotographerDAL.GetPhotographer(2);
-
-            /* old (for reference): TODO: DELETE
-            DataAccessLayer accessLayer = new DataAccessLayer();
-            DataAccessLayer accessSlayer = new DataAccessLayer();
-            PhotographerModel pg = accessLayer.GetPhotographer(88);
-            PhotographerModel pg2 = accessSlayer.GetPhotographer(2);
-            */
-            MyLabel1.Content = "Vorname: " + photographer.Vorname;
-            MyLabel2.Content = "Nachname: " + photographer.Nachname;
-            MyLabel3.Content = "Notizen: " + photographer.Notizen;
-            MyLabel4.Content = "Geburtstag: " + photographer.Geburtstag;
         }
     }
 }
---
+++ PicDB/PicDB/Windows/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using PicDB.Internal;
+using PicDB.Models;
+
+namespace PicDB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
