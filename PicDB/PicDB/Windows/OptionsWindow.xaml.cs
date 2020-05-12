6d02af4de546abd8bfcd3462d167253fa2c1e868
Nils B <44638127+NilsBroer@users.noreply.github.com>
2020-05-12 18:27:44 +0200

    Add 2 funny gifs to the Options tab

--- PicDB/PicDB/Windows/OptionsWindow.xaml.cs
+++ PicDB/PicDB/Windows/OptionsWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using PicDB.Models;
+using WpfAnimatedGif;
 
 namespace PicDB
 {
@@ -8,9 +12,33 @@ namespace PicDB
     /// </summary>
     public partial class OptionsWindow : UserControl
     {
+        private static int _swap = 1;
         public OptionsWindow()
         {
             InitializeComponent();
+            ButtonBase_OnClick(null, null);
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var image = new BitmapImage();
+
+            if (_swap == 1)
+            {
+                image.BeginInit();
+                image.UriSource = new Uri("../images/internal/wip.gif", UriKind.Relative);
+                image.EndInit();
+                _swap = 2;
+            }
+            else
+            {
+                image.BeginInit();
+                image.UriSource = new Uri("../images/internal/nts.gif", UriKind.Relative);
+                image.EndInit();
+                _swap = 1;
+            }
+            ImageBehavior.SetAnimatedSource(SwitchingGif, image);
         }
     }
 }
