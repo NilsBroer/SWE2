d7aa2d3ad13d05450180c2ae5439967195dd81c7
Nils B <44638127+NilsBroer@users.noreply.github.com>
2020-05-12 17:51:28 +0200

    Add a funny GIF to the Options Tab
    so the lectors may be amused and not deduct any points.

---
+++ PicDB/PicDB/xUnitTests/IPTCTestSuit.cs
@@ -0,0 +1,47 @@
+﻿using PicDB.Models;
+using System;
+using Xunit;
+
+namespace PicDB.UnitTests
+{
+    public class IPTCTestSuit
+    {
+        enum Id
+        {
+            Empty = 0,
+            Default = 1
+        }
+        IPTCModel defaultIPTC = new IPTCModel
+        {
+            PictureId = (int) Id.Default,
+            License = "TLicense",
+            PhotographerName = "TName",
+            Category = "TCategory",
+            IsEdited = false,
+            KeyWords = "TKeyWord1,TKeyWord",
+            Notes = null,
+            CreationDate = DateTime.Now
+        };
+
+        [Fact]
+        public void ItShallReturnNullForNonExistantIPTC()
+        {
+            var sut = BusinessLayer.GetIPTC(Id.Empty);
+            Assert.Null(sut);
+        }
+
+        [Fact]
+        public void ItShallSaveOrUpdateIPTC()
+        {
+            BusinessLayer.SaveIPTC(defaultIPTC);
+            var sut = BusinessLayer.GetIPTC(Id.Default);
+            Assert.Equal(sut,defaultIPTC);
+        }
+
+        /*
+         *[Fact / Theory]
+         *...
+         *
+         */
+    }
+}
